fe5f57489227292ec7b84300bd05ed1b25e8b19b
Derek Lim <dereklim3006@gmail.com>
2023-06-01 21:03:28 -0400

    Add function to allow user to play with computer

--- javascript.js
+++ javascript.js
@@ -28,3 +28,28 @@ function getPlayerChoice() {
     return playerSelection;
 }
 console.log(getPlayerChoice());
+
+//create function playRound()
+//playRound() will take parameters playerSelection and computerSelection and determine who wins
+//playRound() will return strings like "You Lose! Paper beats Rock"
+function playRound(playerSelection, computerSelection) {
+    if ((computerSelection === "rock") && (playerSelection === "scissors")){
+        alert("You lose! Rock beats Scissors");
+    } else if ((computerSelection === "rock") && (playerSelection === "paper")) {
+        alert("You win! Paper beats Rock");
+    } else if ((computerSelection === "scissors") && (playerSelection === "rock")) {
+        alert("You win! Rock beats Scissors");
+    } else if ((computerSelection === "scissors") && (playerSelection === "paper")) {
+        alert("You lose! Scissors beats Paper");
+    } else if ((computerSelection === "paper") && (playerSelection === "rock")) {
+        alert("You lose! Paper beats Rock");
+    } else if ((computerSelection === "paper") && (playerSelection === "scissors")) {
+        alert("You win! Scissors beats Paper");
+    } else if (computerSelection === playerSelection) {
+        computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+        alert(`Nobody wins! ${computerSelection} ties ${computerSelection}`);   
+    } else {
+        alert("error")
+    }
+}
+playRound(playerSelection, computerSelection);

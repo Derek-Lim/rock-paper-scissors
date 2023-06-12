5a0f94143032d668078089196901b2c9769f8aec
Derek Lim <dereklim3006@gmail.com>
2023-06-12 17:41:39 -0400

    Add point system

--- javascript.js
+++ javascript.js
@@ -1,26 +1,26 @@
 //create container for playButtons
-const body = document.body
+const body = document.body;
 const playBox = document.createElement('div');
 body.append(playBox);
-playBox.setAttribute('style', 'display: flex; justify-content: space-around; align-items: center; height: 500px;');
+playBox.setAttribute('style', 'display: flex; justify-content: space-around; align-items: flex-end; height: 400px;');
 
 //create three buttons - one for each selection
 const playButton1 = document.createElement('button');
 playButton1.textContent = "Rock";
-playButton1.setAttribute('style', 'height: 300px; width: 300px; font-size: 75px;')
+playButton1.setAttribute('style', 'height: 300px; width: 300px; font-size: 75px;');
 playBox.appendChild(playButton1);
 
 const playButton2 = document.createElement('button');
 playButton2.textContent = "Paper";
-playButton2.setAttribute('style', 'height: 300px; width: 300px; font-size: 75px;')
+playButton2.setAttribute('style', 'height: 300px; width: 300px; font-size: 75px;');
 playBox.appendChild(playButton2);
 
 const playButton3 = document.createElement('button');
 playButton3.textContent = "Scissors";
-playButton3.setAttribute('style', 'height: 300px; width: 300px; font-size: 75px;')
+playButton3.setAttribute('style', 'height: 300px; width: 300px; font-size: 75px;');
 playBox.appendChild(playButton3);
 
-//add event listeners
+//add event listeners to playButtons
 playButton1.addEventListener('click', () => {
     let playerSelection = "rock";
     getComputerChoice();
@@ -39,6 +39,65 @@ playButton3.addEventListener('click', () => {
     playRound(playerSelection, computerSelection);
 })
 
+//create container for point display
+const pointBox = document.createElement('div');
+body.append(pointBox);
+pointBox.setAttribute('style', 'display: flex; justify-content: space-around; margin-top: 150px;');
+
+//create playerPointDisplay
+const playerPointDisplay = document.createElement('div');
+pointBox.append(playerPointDisplay);
+playerPointDisplay.setAttribute('style', 'font-size: 75px;');
+let playerPoints = 0;
+playerPointDisplay.textContent = `Player: ${playerPoints}`;
+
+//create computerPointDisplay
+const computerPointDisplay = document.createElement('div');
+pointBox.append(computerPointDisplay);
+computerPointDisplay.setAttribute('style', 'font-size: 75px;');
+let computerPoints = 0;
+computerPointDisplay.textContent = `Computer: ${computerPoints}`;
+
+//create function playRound()
+//playRound() will take parameters playerSelection and computerSelection and determine who wins
+//playRound() will return strings like "You Lose! Paper beats Rock"
+//playRound() will give points to winner after match
+function playRound(playerSelection, computerSelection) {
+    if ((computerSelection === "rock") && (playerSelection === "scissors")){
+        computerPoints += 1;
+        computerPointDisplay.textContent = `Computer: ${computerPoints}`;
+        alert("You lose! Rock beats Scissors");
+    } else if ((computerSelection === "rock") && (playerSelection === "paper")) {
+        playerPoints += 1;
+        playerPointDisplay.textContent = `Player: ${playerPoints}`;
+        alert("You win! Paper beats Rock");
+    } else if ((computerSelection === "scissors") && (playerSelection === "rock")) {
+        playerPoints += 1;
+        playerPointDisplay.textContent = `Player: ${playerPoints}`;
+        alert("You win! Rock beats Scissors");
+    } else if ((computerSelection === "scissors") && (playerSelection === "paper")) {
+        computerPoints += 1;
+        computerPointDisplay.textContent = `Computer: ${computerPoints}`;
+        alert("You lose! Scissors beats Paper");
+    } else if ((computerSelection === "paper") && (playerSelection === "rock")) {
+        computerPoints += 1;
+        computerPointDisplay.textContent = `Computer: ${computerPoints}`;
+        alert("You lose! Paper beats Rock");
+    } else if ((computerSelection === "paper") && (playerSelection === "scissors")) {
+        playerPoints += 1;
+        playerPointDisplay.textContent = `Player: ${playerPoints}`;
+        alert("You win! Scissors beats Paper");
+    } else if (computerSelection === playerSelection) {
+        computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+        alert(`Nobody wins! ${computerSelection} ties ${computerSelection}`);
+    } else {
+        alert("error");
+    }
+    if (playerPoints === 5 || computerPoints === 5) {
+        announceWinner();
+    }
+}
+
 //create function getComputerChoice()
 //getComputerChoice() will randomly select rock, scissors, or paper
 function getComputerChoice() {
@@ -55,107 +114,18 @@ function getComputerChoice() {
     return computerSelection;
 }
 
-// //create function getPlayerChoice()
-// //getPlayerChoice() will ask user and save the response
-// function getPlayerChoice() {
-//     playerSelection = prompt("Rock, scissors, or paper?");
-//     //make playerSelection case-insensitive
-//     playerSelection = playerSelection.toLowerCase();
-//     //make sure player only selects rock, paper, or scissors
-//     while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-//         playerSelection = prompt(`"${playerSelection}" is not valid. Please pick again.`);
-//         //make playerSelection case-insensitive
-//         playerSelection = playerSelection.toLowerCase();
-//     }
-//     return playerSelection;
-// }
-
-//create function playRound()
-//playRound() will take parameters playerSelection and computerSelection and determine who wins
-//playRound() will return strings like "You Lose! Paper beats Rock"
-//playRound() will give points to winner after match
-let computerPoints = 0;
-let playerPoints = 0;
-function playRound(playerSelection, computerSelection) {
-    if ((computerSelection === "rock") && (playerSelection === "scissors")){
-        alert("You lose! Rock beats Scissors");
-        computerPoints += 1;
-        return computerPoints;
-    } else if ((computerSelection === "rock") && (playerSelection === "paper")) {
-        alert("You win! Paper beats Rock");
-        playerPoints += 1;
-        return playerPoints;
-    } else if ((computerSelection === "scissors") && (playerSelection === "rock")) {
-        alert("You win! Rock beats Scissors");
-        playerPoints += 1;
-        return playerPoints;
-    } else if ((computerSelection === "scissors") && (playerSelection === "paper")) {
-        alert("You lose! Scissors beats Paper");
-        computerPoints += 1;
-        return computerPoints;
-    } else if ((computerSelection === "paper") && (playerSelection === "rock")) {
-        alert("You lose! Paper beats Rock");
-        computerPoints += 1;
-        return computerPoints;
-    } else if ((computerSelection === "paper") && (playerSelection === "scissors")) {
-        alert("You win! Scissors beats Paper");
-        playerPoints += 1;
-        return playerPoints;
-    } else if (computerSelection === playerSelection) {
-        computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
-        alert(`Nobody wins! ${computerSelection} ties ${computerSelection}`);   
-    } else {
-        alert("error")
-    }
-}
-
-//create function game()
-//game() will allow user to play 5 rounds against computer
-//game() will track score
-//game() will announce winner
-function game() {
-    getComputerChoice();
-    getPlayerChoice();
-    playRound(playerSelection, computerSelection);
-    console.log(`Round 1\nComputer: ${computerPoints}\nYou: ${playerPoints}`);
-
-    getComputerChoice();
-    getPlayerChoice();
-    playRound(playerSelection, computerSelection);
-    console.log(`Round 2\nComputer: ${computerPoints}\nYou: ${playerPoints}`);
-
-    getComputerChoice();
-    getPlayerChoice();
-    playRound(playerSelection, computerSelection);
-    console.log(`Round 3\nComputer: ${computerPoints}\nYou: ${playerPoints}`);
-
-    getComputerChoice();
-    getPlayerChoice();
-    playRound(playerSelection, computerSelection);
-    console.log(`Round 4\nComputer: ${computerPoints}\nYou: ${playerPoints}`);
-
-    getComputerChoice();
-    getPlayerChoice();
-    playRound(playerSelection, computerSelection);
-    console.log(`Round 5\nComputer: ${computerPoints}\nYou: ${playerPoints}`);
-
-    announceWinner();
-}
-
 //create function announceWinner()
 //announceWinner will announce winner
 function announceWinner() {
-    if (computerPoints > playerPoints) {
-        alert(`Computer Wins!\nComputer: ${computerPoints}\nYou: ${playerPoints}`);
-    } else if (playerPoints > computerPoints) {
-        alert(`You win!\nComputer: ${computerPoints}\nYou: ${playerPoints}`);
-    } else if (playerPoints === computerPoints) {
-        alert(`Tie!\nComputer: ${computerPoints}\nYou: ${playerPoints}`);
+    if (playerPoints === 5) {
+        alert('You win!');
+    } else if (computerPoints === 5) {
+        alert('You lose!');
     } else {
-        alert(`error\nComputer: ${computerPoints}\nYou: ${playerPoints}`);
+        alert('error');
     }
+    playerPoints = 0;
+    computerPoints = 0;
+    playerPointDisplay.textContent = `Player: ${playerPoints}`;
+    computerPointDisplay.textContent = `Computer: ${computerPoints}`;
 }
-
-//play
-// game();
-
